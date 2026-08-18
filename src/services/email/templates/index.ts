691b4f8457fa09f49b44d84fcc962e5bb6b1e8e3
Jude Besmonte <juddebesmonte@gmail.com>
2026-08-18 22:57:18 +0800

    refactor: schema fix

--- src/services/email/templates/index.ts
+++ src/services/email/templates/index.ts
@@ -1,15 +1,2 @@
 // Welcome email
-export { WelcomeEmail, sendWelcomeEmail } from "./welcome"
-
-// Subscription emails
-export { SubscriptionRequestEmail, sendSubscriptionRequestEmail } from "./subscription-request"
-export { SubscriptionSuccessEmail, sendSubscriptionSuccessEmail } from "./subscription-success"
-export { SubscriptionSuspendedEmail, sendSubscriptionSuspendedEmail } from "./subscription-suspended"
-export { SubscriptionTerminatedEmail, sendSubscriptionTerminatedEmail } from "./subscription-terminated"
-export { InstallationScheduledEmail, sendInstallationScheduledEmail } from "./subscription-installation"
-
-// Payment emails
-export { PaymentSuccessEmail, sendPaymentSuccessEmail } from "./payment-success"
-export { PaymentFailedEmail, sendPaymentFailedEmail } from "./payment-failed"
-export { PaymentDueReminder, sendPaymentDueReminderEmail } from "./payment-due-reminder"
-export { PaymentOverdueNotice, sendPaymentOverdueNoticeEmail } from "./payment-overdue-notice" 
+export { WelcomeEmail, sendWelcomeEmail } from "./welcome" 
